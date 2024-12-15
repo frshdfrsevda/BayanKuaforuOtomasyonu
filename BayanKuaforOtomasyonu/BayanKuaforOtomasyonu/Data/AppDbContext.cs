e40842c55cdf40db4ed6a58262d72c248faa04d8
frshdfrsevda <sevda.fa1997@gmail.com>
2024-12-15 05:12:41 +0300

    Identity kütüphanesi

---
+++ BayanKuaforOtomasyonu/BayanKuaforOtomasyonu/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BayanKuaforOtomasyonu.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace BayanKuaforOtomasyonu.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
---
+++ BayanKuaforOtomasyonu/BayanKuaforOtomasyonu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BayanKuaforOtomasyonu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
