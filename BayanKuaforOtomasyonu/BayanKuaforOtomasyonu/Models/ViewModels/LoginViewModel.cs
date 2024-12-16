6e9de51bdfeabbd63c83f0ff2c776143c1258203
frshdfrsevda <sevda.fa1997@gmail.com>
2024-12-16 17:34:07 +0300

    Authentication ViewModels

---
+++ BayanKuaforOtomasyonu/BayanKuaforOtomasyonu/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BayanKuaforOtomasyonu.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [EmailAddress(ErrorMessage ="Email format is wrong!")]
+        [Display(Name = "Email Address*")]
+        public string EmailAddress { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Password field is required!")]
+        [Display(Name ="Password*")]
+        public string Password { get; set; }
+    }
+}
